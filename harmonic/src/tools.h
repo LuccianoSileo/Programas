b0c89af03b3bea3219b3cd23cf229bfc9e03329e
Darkroos <alu0101501253@ull.edu.es>
2022-11-19 14:32:23 +0000

    Probando

---
+++ harmonic/src/tools.h
@@ -0,0 +1,15 @@
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Informática Básica 2022-2023
+  *
+  * @file 
+  * @author Lucciano Sileo alu0101501253@ull.edu.es
+  * @date Oct 12 2022
+  * @brief 
+  * @bug There are no known bugs
+  * @see 
+  **/
+
+#include <iostream>
