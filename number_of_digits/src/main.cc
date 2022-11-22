0f0ba2347e48c1bac27f47be909474e6ca43087f
LuccianoSileo <alu0101501253@ull.edu.es>
2022-11-22 11:18:33 +0000

    Probando

--- number_of_digits/src/main.cc
+++ number_of_digits/src/main.cc
@@ -1,17 +1,25 @@
 /**
-  * Universidad de La Laguna
-  * Escuela Superior de Ingeniería y Tecnología
-  * Grado en Ingeniería Informática
-  * Informática Básica 2022-2023
-  *
-  * @file 
-  * @author Lucciano Sileo alu0101501253@ull.edu.es
-  * @date Oct 12 2022
-  * @brief 
-  * @bug There are no known bugs
-  * @see 
-  **/
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica 2022-2023
+ *
+ * @file
+ * @author Lucciano Sileo alu0101501253@ull.edu.es
+ * @date Oct 12 2022
+ * @brief
+ * @bug There are no known bugs
+ * @see
+ **/
 
 #include <iostream>
 
 #include "tools.h"
+
+int main() {
+  int a;
+  std::cin >> a;
+  std::cout << "caca" << std::endl;
+  std::cout << "The number of digits of " << a << " is " << counter(a) << ".\n";
+  return 0;
+}
